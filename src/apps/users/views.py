9ddcc174b70a3b0cf6fa9f760af1e946615f7696
ziyodullo <ziyodullodev@gmail.com>
2026-03-07 20:34:43 +0500

    add: views and fix login template

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def dashboard_page(request):
+
+    if not request.user.is_authenticated:
+        return redirect("login_view")
+
+    return render(request, "dashboard.html")
+
+
+def login_page(request):
+    return render(request, "login.html")
