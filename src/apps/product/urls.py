c9fc602df55015381e37a0d17e4526fdb369e717
a85274220-ai <a85274220@gmail.com>
2026-03-10 20:13:21 +0500

    add: products and products create pages

--- src/apps/product/urls.py
+++ src/apps/product/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('monitoring/', views.monitoring_page, name="monitoring_page"),
+    path('products/', views.products_page, name="products_page"),
+    path('products/create/', views.products_create_page, name="products_create_page"),
 ]
--- src/apps/product/views.py
+++ src/apps/product/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def monitoring_page(request):
     return render(request, "monitoring.html")
+
+
+def products_page(request):
+    return render(request, "products.html")
+
+
+def products_create_page(request):
+    return render(request, "products-create.html")
