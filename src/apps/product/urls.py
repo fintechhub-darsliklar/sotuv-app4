5e3e5ac5fd1ba56855ba254705af18311ffbcb71
a85274220-ai <a85274220@gmail.com>
2026-03-10 19:57:29 +0500

    add: monitoring page

---
+++ src/apps/product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('monitoring/', views.monitoring_page, name="monitoring_page"),
+]
--- src/apps/product/views.py
+++ src/apps/product/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def monitoring_page(request):
+    return render(request, "monitoring.html")
